0ac7fa89352a4d4a01ebd1b5bb5f870bbb64ee89
rossan <gr.rossan@gmail.com>
2022-11-08 15:49:26 -0300

    FEAT: Home

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var oc = false;
+ OpenCloseMenu = () =>{
+     // let menu = document.getElementById('menu');
+     oc  = !oc
+     if(oc){menu.style = 'margin-left:0px';}
+     else { menu.style = 'margin-left: -100%';}
+    console.log(oc)
+ }
