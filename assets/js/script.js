7d07e0ec4581728a567c0505c645ad06c2245c39
rossan <gr.rossan@gmail.com>
2022-11-09 17:02:10 -0300

    Telas: Cobranças

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var oc = false;
  OpenCloseMenu = () =>{
      // let menu = document.getElementById('menu');
      oc  = !oc
-     if(oc){menu.style = 'margin-left:0px';}
-     else { menu.style = 'margin-left: -100%';}
+     if(oc){menu.style = 'margin-left:0px; top:0px; box-shadow: 200px 0px 0px 0px #0000009e;';}
+     else { menu.style = 'margin-left: -100%; top:0px; box-shadow: none';}
     console.log(oc)
  }
